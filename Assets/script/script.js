cfca665ef0db5da9b4b078fcdcd0b1e6d00475e0
MJ-AZN <michaeljibarra63@gmail.com>
2023-10-20 01:21:48 -0700

    Finished writing questions

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -7,6 +7,46 @@ function startQuiz(){
     console.log("You are sane")
     startButton.classList.add("hide")
     quiz.classList.remove("hide")
-    setNextQuestion()
+    nextQuestion()
 }
 
+function nextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
+
+const questions = [
+    {
+        index: "0",
+        question: "What airport serves the Greater Los Angeles?",
+        answers: ["SNA", "LAX", "ONT", "LGB"],
+        correct: "LAX"
+    },
+    {
+        index: "1",
+        question: "Where is DBX?",
+        answers: ["Denmark", "Dublin", "Dubai", "Durham"],
+        correct: "Dubai"
+    },
+    {
+        index: "2",
+        question: "What airport is the busiest in the United States?",
+        answers: ["ATL", "JFK!", "LAX", "ORD"],
+        correct: "ATL"
+    },
+    {
+        index: "3",
+        question: "Which of these airports is closest to central Tokyo?",
+        answers: ["KIX", "ITM", "NRT", "HND"],
+        correct: "HND"
+    },
+    {
+        index: "4",
+        question: "What city does CDG serve?",
+        answers: ["Copenhagen", "Frankfurt", "Paris", "Sydney"],
+        correct: "Paris"
+    }
+]
